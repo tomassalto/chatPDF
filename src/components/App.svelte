4847aa34c80b15ab8a5fe6f9017b1de9ee3741e2
Tomás Salto <tomasagustinsalto@gmail.com>
2024-03-21 21:17:03 -0300

    advances AI

---
+++ src/components/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Alert } from "flowbite-svelte";
+  import { APP_STATUS, appStatus } from "../store";
+  import SetpUpload from "./SetpUpload.svelte";
+  import SetpLoading from "./SetpLoading.svelte";
+  import StepChat from "./StepChat.svelte";
+</script>
+
+{#if $appStatus === APP_STATUS.INIT}
+  <SetpUpload />
+{:else if $appStatus == APP_STATUS.LOADING}
+  <SetpLoading />
+{:else if $appStatus == APP_STATUS.ERROR}
+  <Alert>
+    <span class="font-medium">Algo malo ha pasado!</span>
+    Hay un error en la app.
+  </Alert>
+{:else if $appStatus == APP_STATUS.CHAT_MODE}
+  <StepChat />
+{:else}
+  <Alert>
+    <span class="font-medium">Accion no reconocida</span>
+    Este estado de la app no esta reconocido.
+  </Alert>
+{/if}
---
+++ src/components/SetpLoading.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Spinner } from "flowbite-svelte";
+</script>
+
+<div class="flex justify-center items-center flex-col gap-y-2">
+  <Spinner />
+  <p class="opacity-70">Loading...</p>
+</div>
